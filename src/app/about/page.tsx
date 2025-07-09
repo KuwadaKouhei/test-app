46c414e33bcaf03cdab34dc72a883425b0b31ba1
Kuwada <kuwada.k3205@gmail.com>
2025-07-10 01:38:56 +0900

    test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
 export default function About() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex gap-4 mx-4">
+      <div className="w-1/2 flex flex-col gap-1">
+        <div className="bg-green-400">Column Left Row 1</div>
+        <div className="bg-green-400">Column Left Row 2</div>
+        <div className="bg-green-400">Column Left Row 3</div>
+      </div>
+      <div className="w-1/2  flex flex-col gap-1">
+        <div className="bg-blue-500">Column Right Row 1</div>
+        <div className="bg-blue-500">Column Right Row 2</div>
+        <div className="bg-blue-500">Column Right Row 3</div>
+      </div>
+    </div>
   );
 }
